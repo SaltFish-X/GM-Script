fe00761717c2b9416f58f03db8b7d26610824abb
X-bear <522168288@qq.com>
2024-12-03 14:26:58 +0800

    重写了表格生成函数

--- ☆看帖：PAUSE EA 持久版.js
+++ ☆看帖：PAUSE EA 持久版.js
@@ -273,12 +273,6 @@
         var creditHistoryStr = localStorage.getItem('extractedCreditHistory');
         var creditHistory = JSON.parse(creditHistoryStr);
 
-        //创建记录表格
-        var tableHTML = '';
-
-        // 构建表格的HTML字符串
-        tableHTML += '<table border="1"><thead><tr><th>行号</th><th>奖励类型</th><th>是否触发</th><th>分区</th><th>旅程</th><th>金币</th><th>血液</th><th>咒术</th><th>知识</th><th>灵魂</th><th>堕落</th><th>时间</th><th>灵魂期望</th></tr></thead><tbody>';
-
         //创建行号
         var rowNumber = 1;
         var tempLvCheng = 0;
@@ -296,34 +290,7 @@
                 if (checkItem(item)) {
                     return;
                 }
-                checkCreditHistory.push(item)
-
-                // 解析ISO 8601时间字符串为UTC时间，然后转为本地时间
-                var date = new Date(item.acquiredAt);
-
-                // 格式化日期和时间
-                var formattedDateTime = date.getFullYear() + '-' +
-                    ('0' + (date.getMonth() + 1)).slice(-2) + '-' +
-                    ('0' + date.getDate()).slice(-2) + ' ' +
-                    ('0' + date.getHours()).slice(-2) + ':' +
-                    ('0' + date.getMinutes()).slice(-2) + ':' +
-                    ('0' + date.getSeconds()).slice(-2);
-
-                tableHTML +=
-                    `<tr><td>${rowNumber++}</td>
-                        <td>${item.creditType}</td>
-                        <td>${item.badgeActivated}</td>
-                        <td>${item.area || '暂无数据'}</td>
-                        <td>${item.lvCheng}</td>
-                        <td>${item.jinBi}</td>
-                        <td>${item.xueYe}</td>
-                        <td>${item.zhouShu}</td>
-                        <td>${item.zhiShi}</td>
-                        <td>${item.lingHun}</td>
-                        <td>${item.duoLuo}</td>
-                        <td>${formattedDateTime}</td>
-                        <td>${linghunExpectationsFormat(item.linghunExpectations)}</td>
-                        </tr>`;
+                checkCreditHistory.push({ ...item, rowNumber })
 
                 tempLvCheng += item.lvCheng;
                 temmpJinBi += item.jinBi;
@@ -333,29 +300,37 @@
                 tempLingHun += item.lingHun;
                 tempDuoLuo += item.duoLuo;
 
+                rowNumber++
             });
         }
-        tableHTML += '</tbody></table>';
 
-        var summaryTableHTML = ''
-        summaryTableHTML += '<table id="summaryTable" border="1"><thead><tr><th>行数</th><th>旅程</th><th>金币</th><th>血液</th><th>咒术</th><th>知识</th><th>灵魂</th><th>堕落</th></thead><tbody>';
-        summaryTableHTML +=
-            `<tr><td>${--rowNumber}</td>
-                        <td>${tempLvCheng}</td>
-                        <td>${temmpJinBi}</td>
-                        <td>${tempXueYe}</td>
-                        <td>${tempZhouShu}</td>
-                        <td>${tempZhiShi}</td>
-                        <td>${tempLingHun}</td>
-                        <td>${tempDuoLuo}</td></tr>`;
+        // 格式化日期和时间
+        // 解析ISO 8601时间字符串为UTC时间，然后转为本地时间
+        var formattedDateTime = (date) => {
+            var date = new Date(date);
+            return date.getFullYear() + '-' +
+                ('0' + (date.getMonth() + 1)).slice(-2) + '-' +
+                ('0' + date.getDate()).slice(-2) + ' ' +
+                ('0' + date.getHours()).slice(-2) + ':' +
+                ('0' + date.getMinutes()).slice(-2) + ':' +
+                ('0' + date.getSeconds()).slice(-2);
+        }
+        const headers = ['行号', '奖励类型', '是否触发', '分区', '旅程', '金币', '血液', '咒术', '知识', '灵魂', '堕落', '时间', '灵魂期望'];
+        const dataKeys = ['rowNumber', 'creditType', 'badgeActivated', 'area', 'lvCheng', 'jinBi', 'xueYe', 'zhouShu', 'zhiShi', 'lingHun', 'duoLuo', 'acquiredAt', 'linghunExpectations'];
+        const dataFormat = {
+            acquiredAt: val => formattedDateTime(val),
+            linghunExpectations: item => linghunExpectationsFormat(item)
+        };
+        const mainTable = generateTable(checkCreditHistory, headers, dataKeys, dataFormat, true)
 
+        // 小计表格
+        const summaryTable = generateSummaryTable([{ rowNumber, tempLvCheng, temmpJinBi, tempXueYe, tempZhouShu, tempZhiShi, tempLingHun, tempDuoLuo }])
         // 计算分区 并 生成回帖数表格
         const areaNum = getAreaNum(checkCreditHistory)
         const areaTable = generateAreaTable(areaNum)
-        console.log(areaNum);
 
         // 整合页面HTML
-        var rightHTML = '<h3>当前记录汇总</h3>' + summaryTableHTML + areaTable + tableHTML;
+        var rightHTML = '<h3>当前记录汇总</h3>' + summaryTable + areaTable + mainTable;
 
         return rightHTML;
     }
@@ -892,29 +867,19 @@
         })
         return result
     }
-    // 生产计算分区回帖数表格
+    // 生成计算分区回帖数表格
     function generateAreaTable(data) {
-        let tableHTML = '<table>';
+        const newdata = [{ '回帖数': '', ...data }]
+        const headers = Object.keys(data)
+        const dataKeys = headers
+        return generateTable(newdata, headers, dataKeys, {}, true)
+    }
 
-        // 添加表头
-        tableHTML += '<thead><tr>';
-        tableHTML += '<th></th>'; // 空白单元格
-        for (const key in data) {
-            tableHTML += `<th>${key}</th>`;
-        }
-        tableHTML += '</tr></thead>';
-
-        // 添加表体
-        tableHTML += '<tbody><tr>';
-        tableHTML += '<td>回帖数</td>'; // 标签单元格
-        for (const key in data) {
-            tableHTML += `<td>${data[key]}</td>`;
-        }
-        tableHTML += '</tr></tbody>';
-
-        tableHTML += '</table>';
-
-        return tableHTML;
+    // 生成总计
+    function generateSummaryTable(data) {
+        const headers = ['行数', '旅程', '金币', '血液', '咒术', '知识', '灵魂', '堕落']
+        const dataKeys = ['rowNumber', 'tempLvCheng', 'temmpJinBi', 'tempXueYe', 'tempZhouShu', 'tempZhiShi', 'tempLingHun', 'tempDuoLuo']
+        return generateTable(data, headers, dataKeys, { rowNumber: (val) => val - 1 }, true)
     }
 
     GM_registerMenuCommand('更新灵魂期望', () => {
@@ -979,5 +944,65 @@
         const outputString = `${total.toFixed(2)} = ${outputParts.join(' + ')}`;
 
         return outputString
+    }
+    
+    /**
+     * 生成一个 HTML 表格。
+     *
+     * @param {Array<Object>} data - 表格数据的数组，每个对象代表一行数据。
+     * @param {Array<string>} headers - 表头的名称数组，用于定义表格的列标题。
+     * @param {Array<string>} dataKeys - 数据对象中对应的键名数组，用于从数据中提取显示的值。
+     * @param {Object} [dataFormat] - 可选参数，包含格式化函数的对象，格式化每个字段的显示值。
+     * @param {boolean} [inHTML=false] - 可选参数，若为 true，则返回 HTML 字符串形式的表格；否则返回 DOM 元素。
+     * 
+     * @returns {HTMLElement|string} 返回生成的表格元素或 HTML 字符串，具体取决于 `inHTML` 参数的值。
+     *
+     * @example
+     * const data = [{lvcheng: 1, jinbi: 10}];
+     * const headers = ['旅程', '金币'];
+     * const dataKeys = ['lvcheng', 'jinbi'];
+     * const tableElement = generateTable(data, headers, dataKeys);
+     * document.body.appendChild(tableElement);
+     */    
+    function generateTable(data, headers, dataKeys, dataFormat, inHTML) {
+        if (dataFormat) {
+            data = data.map(item => {
+                const formattedItem = {};
+                for (const key of dataKeys) {
+                    if (dataFormat[key]) {
+                        formattedItem[key] = dataFormat[key](item[key]);
+                    } else {
+                        formattedItem[key] = item[key];
+                    }
+                }
+                return formattedItem;
+            });
+        }
+
+        let tableHTML = '<table><thead><tr>';
+
+        headers.forEach(header => {
+            tableHTML += `<th>${header}</th>`;
+        });
+
+        tableHTML += '</tr></thead><tbody>';
+
+        data.forEach(item => {
+            tableHTML += '<tr>';
+            dataKeys.forEach(key => {
+                tableHTML += `<td>${item[key] !== undefined ? item[key] : ''}</td>`;
+            });
+            tableHTML += '</tr>';
+        });
+
+        tableHTML += '</tbody></table>';
+
+        if (inHTML) {
+            return tableHTML; // 返回 HTML 字符串
+        } else {
+            const table = document.createElement('div');
+            table.innerHTML = tableHTML; // 将 HTML 字符串插入到一个 div 中
+            return table.firstChild; // 返回生成的 table 元素
+        }
     }
 })();
