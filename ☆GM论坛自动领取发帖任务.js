f055b2ce5c11244c6976b5653bfad936fc5c4057
X-bear <522168288@qq.com>
2025-01-11 08:44:48 +0800

    Update ☆GM论坛自动领取发帖任务.js

--- ☆GM论坛自动领取发帖任务.js
+++ ☆GM论坛自动领取发帖任务.js
@@ -1,48 +1,19 @@
 // ==UserScript==
 // @name         GM论坛自动领取发帖任务
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  自动领取发帖任务
-// @match        https://www.gamemale.com/forum.php
-// @match        https://www.gamemale.com/thread-*
 // @match        https://www.gamemale.com/forum.php?mod=viewthread&tid=*
+// @match        https://www.gamemale.com/forum.php?mod=post&action=newthread&fid=*
 // @grant        none
 // @icon         https://www.gamemale.com/template/mwt2/extend/img/favicon.ico
 // @license      GPL
 // ==/UserScript==
 // 每周发帖任务的刷新时间实际为每周日00：00，并不是每周一
 
-// 下载地址
-// https://greasyfork.org/zh-CN/scripts/519502-gm%E8%AE%BA%E5%9D%9B%E8%87%AA%E5%8A%A8%E9%A2%86%E5%8F%96%E5%8F%91%E5%B8%96%E4%BB%BB%E5%8A%A1
+// 下载地址 https://greasyfork.org/zh-CN/scripts/519502
 
 (function () {
-  // 获取当前日期
-  const currentDateString = getDate()
-  // 检查localStorage中存储的最后执行日期
-  const lastExecutionDate = localStorage.getItem('每周发帖任务标记');
-
-  if (lastExecutionDate === currentDateString) {
-    console.log('今天已经自动领取发帖任务');
-  } else {
-    applyTask()
-    claimReward()
-
-    localStorage.setItem('每周发帖任务标记', currentDateString);
-  }
-
-
-  // 获取当前时间的年、月和日
-  function getDate() {
-    const currentDate = new Date();
-
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1; // 月份从0开始，需要加1
-    const day = currentDate.getDate();
-
-    const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
-    return formattedDate
-  }
-
   // 申请任务
   // https://www.gamemale.com/home.php?mod=task&do=apply&id=25
 
@@ -63,21 +34,43 @@
     cancel: '您已放弃该任务，您还可以继续完成其他任务或者申请新任务'
   }
 
+  // 发新帖 https://www.gamemale.com/forum.php?mod=post&action=newthread&fid=53
+  // 编辑原有帖子 https://www.gamemale.com/forum.php?mod=post&action=edit&fid=150&tid=112806&pid=4755585&page=7
+  // 回复帖子 https://www.gamemale.com/forum.php?mod=post&action=reply&fid=54&tid=152292
+
   // 自动领取发帖任务
   // TODO 领任务可以改成进入发帖页面的时候再去领，可以加个标识方便判断
+  // 获取当前页面的 URL
+  const currentUrl = window.location.href
+  const currentReferrer = document.referrer
+  const newThreadUrl = 'https://www.gamemale.com/forum.php?mod=post&action=newthread&fid='
+
+  // 检查当前 URL 是否匹配正则表达式
+  if (currentUrl.includes(newThreadUrl)) {
+    // 如果匹配，执行 applyTask 函数
+    applyTask()
+    console.log('已经领取每周发帖任务')
+  }
+
+  // 检查 referrer 是否匹配
+  if (currentReferrer.includes(newThreadUrl)) {
+    claimReward() // 调用你的函数
+    console.log('已经领取每周发帖奖励')
+  }
+
   function applyTask() {
     return fetch('https://www.gamemale.com/home.php?mod=task&do=apply&id=25', {
       method: 'GET'
     })
       .then(response => response.text())
       .then(html => {
-        const parser = new DOMParser();
+        const parser = new DOMParser()
         const doc = parser.parseFromString(html, 'text/html')
         const messagetext = doc.querySelector('#messagetext p').textContent
-        console.log(messagetext);
+        console.log(messagetext)
       })
       .catch(error => {
-        console.error('发生错误：', error);
+        console.error('发生错误：', error)
       })
   }
 
@@ -92,10 +85,10 @@
       method: 'GET'
     }).then(response => response.text())
       .then(html => {
-        const parser = new DOMParser();
+        const parser = new DOMParser()
         const doc = parser.parseFromString(html, 'text/html')
         const messagetext = doc.querySelector('#messagetext p').textContent
-        console.log(messagetext);
+        console.log(messagetext)
 
         if (messagetext.includes(messageInfo.done)) {
           // 清除系统消息
@@ -103,7 +96,7 @@
         }
       })
       .catch(error => {
-        console.error('发生错误：', error);
+        console.error('发生错误：', error)
       })
   }
 
@@ -113,54 +106,14 @@
 
   // 发帖
   // https://www.gamemale.com/forum.php?mod=post&action=newthread&fid=206
-  // https://www.gamemale.com/forum.php?mod=post&action=newthread&fid=154
-  // https://www.gamemale.com/forum.php?mod=viewthread&tid=150221
+
   // 定义目标页面的正则匹配规则，忽略额外的查询参数
+  // https://www.gamemale.com/forum.php?mod=viewthread&tid=150221
 
   // 不知道为什么发帖成功后会有两个界面，非常奇怪
   // https://www.gamemale.com/thread-*
   // https://www.gamemale.com/forum.php?mod=viewthread&tid=*
 
-  // 检查 referrer 是否匹配
-  if (/^https:\/\/www\.gamemale\.com\/forum\.php\?mod=post&action=newthread(&.*)?/.test(document.referrer)) {
-    claimReward(); // 调用你的函数
-    console.log('已经领取每周发帖奖励');
-  }
-
   // 可以监听标签，但是算了，有点麻烦，重复造轮子，我想弄点简单的
   // 我选择正则+document.referrer
-  function startObserve() {
-    const targetNode = document.getElementById('append_parent');
-
-    // 观察器配置
-    const config = { attributes: false, childList: true, subtree: false };
-
-    // 当检测到变化时调用的回调函数
-    const callback = function () {
-
-      // 如果检测到奖励内容再执行函数
-      if (document.getElementById("creditpromptdiv")) {
-
-        var creditElement = document.getElementById("creditpromptdiv");
-        if (creditElement) {
-          const creditTypeNode = creditElement.querySelector('i');
-          console.log(creditTypeNode);
-          const parts = creditTypeNode.textContent.trim().split(' ');
-          const creditType = parts[0]
-
-          if (creditType === '发表主题') {
-            claimReward()
-          }
-        }
-
-      }
-
-    };
-
-    // 创建一个观察器实例并传入回调函数
-    const observer = new MutationObserver(callback);
-
-    // 开始观察目标节点
-    observer.observe(targetNode, config);
-  }
 })()
