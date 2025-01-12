bc6b730b2c54a6d15619fe5ca8ae299f61952f04
X-bear <522168288@qq.com>
2025-01-12 17:12:20 +0800

    Update ☆GM论坛勋章百宝箱.js

--- ☆GM论坛勋章百宝箱.js
+++ ☆GM论坛勋章百宝箱.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GM论坛勋章百宝箱
 // @namespace    http://tampermonkey.net/
-// @version      2.3.8
+// @version      2.3.8.1
 // @description  主要用于管理GM论坛的个人勋章，查看其他勋章属性请下载【勋章放大镜】
 // @match        https://www.gamemale.com/wodexunzhang-showxunzhang.html?action=my
 // @grant        GM_addStyle
