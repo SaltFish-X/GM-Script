3089590bee96d90faf739b2641b2dbcab80217a6
X-bear <522168288@qq.com>
2025-04-14 19:29:57 +0800

    Update ☆GM论坛勋章百宝箱.js

--- ☆GM论坛勋章百宝箱.js
+++ ☆GM论坛勋章百宝箱.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GM论坛勋章百宝箱
 // @namespace    http://tampermonkey.net/
-// @version      2.5.4
+// @version      2.5.5
 // @description  主要用于管理GM论坛的个人勋章，查看其他勋章属性请下载【勋章放大镜】
 // @match        https://www.gamemale.com/wodexunzhang-showxunzhang.html?action=my
 // @match        https://www.gamemale.com/plugin.php?id=wodexunzhang:showxunzhang&action=my
@@ -667,6 +667,8 @@
             "神奇宝贝大师球",
             "神奇宝贝图鉴",
             "猫咪点唱机㊊",
+            "肉乖乖",
+            "灵魂残絮聚合法",
         ],
     }
     // 2025年元旦活动新增的类别，期间限定的临时活动勋章
