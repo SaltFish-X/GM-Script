2ddf1c9cd7740aeb534fbd630cdad8c8770e2899
X-bear <522168288@qq.com>
2025-05-17 16:30:08 +0800

    Update ☆GM论坛勋章放大镜.js

--- ☆GM论坛勋章放大镜.js
+++ ☆GM论坛勋章放大镜.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         勋章放大镜
 // @namespace    http://tampermonkey.net/
-// @version      2.5.0
+// @version      2.5.1
 // @description  泥潭勋章属性展示！
 // @author       轶致
 // @match        https://www.gamemale.com/wodexunzhang-showxunzhang.html*
