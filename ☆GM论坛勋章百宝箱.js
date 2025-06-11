bfa24c4709a24308bf6122be67e2c31f747425c6
X-bear <522168288@qq.com>
2025-06-11 12:11:03 +0800

    Update ☆GM论坛勋章百宝箱.js

--- ☆GM论坛勋章百宝箱.js
+++ ☆GM论坛勋章百宝箱.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GM论坛勋章百宝箱
 // @namespace    http://tampermonkey.net/
-// @version      2.5.9
+// @version      2.5.10
 // @description  主要用于管理GM论坛的个人勋章，查看其他勋章属性请下载【勋章放大镜】
 // @match        https://www.gamemale.com/wodexunzhang-showxunzhang.html?action=my
 // @match        https://www.gamemale.com/plugin.php?id=wodexunzhang:showxunzhang&action=my
@@ -592,6 +592,7 @@
             "『转生经筒』",
             "『林中过夜』",
             "『厢庭望远』",
+            "『狄文卡德的残羽』",
         ],
         "Prize": [
             "深渊遗物",
