2abfd1631f7748f44b3c2c8037e0f21497ef4c35
X-bear <522168288@qq.com>
2025-12-08 20:09:17 +0800

    Update ☆GM论坛勋章百宝箱.js

--- ☆GM论坛勋章百宝箱.js
+++ ☆GM论坛勋章百宝箱.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GM论坛勋章百宝箱
 // @namespace    http://tampermonkey.net/
-// @version      2.6.4
+// @version      2.6.6
 // @description  主要用于管理GM论坛的个人勋章，查看其他勋章属性请下载【勋章放大镜】
 // @match        https://www.gamemale.com/wodexunzhang-showxunzhang.html?action=my
 // @match        https://www.gamemale.com/plugin.php?id=wodexunzhang:showxunzhang&action=my
@@ -713,6 +713,10 @@
             "桂花米糕",
             "德罗瑞安",
             "悬浮滑板",
+            "发条八音盒",
+            "弗雷迪玩偶",
+            "河豚寿司",
+            "荧光水母",
         ],
     }
     // 2025年元旦活动新增的类别，期间限定的临时活动勋章
